1c8e8121559799c47bc4446094d10808db918820
Artem Medvedev <i@ddtkey.com>
2024-01-23 21:11:32 +0100

    feat: introduce ability to read query params and update them (#2)

--- src/pages/buckets_explorer_page.rs
+++ src/pages/buckets_explorer_page.rs
@@ -2,22 +2,40 @@ use crate::components::buckets_chart::BucketsChart;
 use crate::components::buckets_table::BucketsTable;
 use crate::components::input::NumberInput;
 use crate::types::Buckets;
-use leptos::{component, create_signal, view, IntoView, SignalGet, SignalSet};
+use leptos::{component, create_signal, view, IntoView, SignalGet, SignalGetUntracked, SignalSet};
+use leptos_router::use_query;
+use leptos_router::Params;
 
 const DEFAULT_INITIAL_VALUE: f64 = 1.0;
 const DEFAULT_FACTOR: f64 = 1.5;
 const DEFAULT_BUCKETS_NUM: u32 = 15;
 
+#[derive(leptos::Params, Default, Debug, Copy, Clone, PartialEq)]
+struct BucketsQueryParams {
+    initial_value: Option<f64>,
+    factor: Option<f64>,
+    buckets_num: Option<u32>,
+}
+
 #[component]
 pub(crate) fn BucketsExplorerPage() -> impl IntoView {
-    let (initial_value, set_initial_value) = create_signal(DEFAULT_INITIAL_VALUE);
-    let (factor, set_factor) = create_signal(DEFAULT_FACTOR);
-    let (buckets_num, set_buckets_num) = create_signal(DEFAULT_BUCKETS_NUM);
+    let query = use_query::<BucketsQueryParams>()
+        .get_untracked()
+        .unwrap_or_else(|e| {
+            log::debug!("Failed to parse query params: {e}, using default values instead");
+            Default::default()
+        });
+
+    let (initial_value, set_initial_value) =
+        create_signal(query.initial_value.unwrap_or(DEFAULT_INITIAL_VALUE));
+    let (factor, set_factor) = create_signal(query.factor.unwrap_or(DEFAULT_FACTOR));
+    let (buckets_num, set_buckets_num) =
+        create_signal(query.buckets_num.unwrap_or(DEFAULT_BUCKETS_NUM));
 
     let (buckets, set_buckets) = create_signal(Buckets::calculate(
-        DEFAULT_INITIAL_VALUE,
-        DEFAULT_FACTOR,
-        DEFAULT_BUCKETS_NUM,
+        initial_value.get_untracked(),
+        factor.get_untracked(),
+        buckets_num.get_untracked(),
     ));
 
     let update_buckets_callback = move |_| {
