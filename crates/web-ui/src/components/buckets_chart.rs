0867c8249a6f6d6494e456e5d1a455955a8f0b82
Artem Medvedev <i@ddtkey.com>
2024-01-25 03:57:40 +0100

    feat: switch to `charts-js-rs` (#1)

--- crates/web-ui/src/components/buckets_chart.rs
+++ crates/web-ui/src/components/buckets_chart.rs
@@ -1,8 +1,9 @@
 use crate::types::Buckets;
-use charming::component::Axis;
-use charming::element::AxisType;
-use charming::series::Line;
-use charming::{Chart, WasmRenderer};
+use chart_js_rs::scatter::Scatter;
+use chart_js_rs::{
+    ChartOptions, ChartScale, Dataset, DatasetDataExt, NoAnnotations, XYDataset, XYPoint,
+};
+use std::collections::HashMap;
 
 use leptos::{component, create_effect, view, IntoView, ReadSignal, SignalWith};
 
@@ -14,24 +15,53 @@ pub(crate) fn BucketsChart(buckets: ReadSignal<Buckets>) -> impl IntoView {
     create_effect(move |_| {
         buckets.with(|buckets| {
             log::debug!("re-rendering buckets chart");
-            let chart = Chart::new()
-                .x_axis(Axis::new().type_(AxisType::Value))
-                .y_axis(Axis::new().type_(AxisType::Value))
-                .series(
-                    Line::new().data(
-                        buckets
+            let chart: Scatter<NoAnnotations> = Scatter {
+                id: id.to_string(),
+                options: ChartOptions {
+                    scales: Some(HashMap::from([
+                        (
+                            "x".into(),
+                            ChartScale {
+                                r#type: "linear".into(),
+                                ..Default::default()
+                            },
+                        ),
+                        (
+                            "y".into(),
+                            ChartScale {
+                                r#type: "linear".into(),
+                                ..Default::default()
+                            },
+                        ),
+                    ])),
+                    ..Default::default()
+                },
+                data: Dataset {
+                    labels: None,
+                    datasets: Vec::from([XYDataset {
+                        r#type: "line".into(),
+                        label: "Buckets distribution".into(),
+                        data: buckets
                             .iter()
-                            .map(|b| vec![f64::from(b.number()), b.start()])
-                            .collect::<Vec<_>>(),
-                    ),
-                );
+                            .map(|b| XYPoint {
+                                x: b.number().into(),
+                                y: b.start().into(),
+                                description: String::new(),
+                            })
+                            .collect::<Vec<_>>()
+                            .to_dataset_data(),
+                        ..Default::default()
+                    }]),
+                },
+                ..Default::default()
+            };
 
-            let renderer = WasmRenderer::new(600, 400);
-            renderer.render(id, &chart).unwrap();
+            chart.to_chart().render();
+            log::debug!("re-rendering finished");
         })
     });
 
     view! {
-        <div id={id}></div>
+        <canvas id={id}></canvas>
     }
 }
