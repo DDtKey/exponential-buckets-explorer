756b854a05c34df6bcbcaf2328a72a50fe66adab
Artem Medvedev <i@ddtkey.com>
2024-01-26 01:56:22 +0100

    feat: improve styling of the table

--- crates/web-ui/src/components/buckets_table.rs
+++ crates/web-ui/src/components/buckets_table.rs
@@ -18,7 +18,7 @@ pub(crate) fn BucketsTable(buckets: ReadSignal<Buckets>) -> impl IntoView {
                 .map(|bucket| {
                     view! {
                         <tr>
-                            <th class="text-start" scope="col">{bucket.number()}</th>
+                            <th class="text-start" scope="row">{bucket.number()}</th>
                             <td class="text-start" scope="col" title={bucket.le()}>{rounded_float_format.format(bucket.le())}</td>
                         </tr>
                     }
@@ -32,7 +32,7 @@ pub(crate) fn BucketsTable(buckets: ReadSignal<Buckets>) -> impl IntoView {
             <table class="table table-striped table-sm">
                 <thead>
                     <tr>
-                        <th class="text-start" scope="row">#</th>
+                        <th class="text-start" scope="col">#</th>
                         <th class="text-start" scope="col">Less than or equal to</th>
                     </tr>
                 </thead>
