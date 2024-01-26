535cd74da601d3b9442e536be74fbac2d66caf57
Artem Medvedev <i@ddtkey.com>
2024-01-26 01:27:22 +0100

    fix: buckets calculations (#9)
    It's hard to represent buckets as ranges, because of actual rule is:
    bucket is less than or equal to the value, but greater than previous
    bucket.
    
    But for first bucket the initial value is 0 which is inclusive.
    
    Need to think how improve the UX

--- crates/web-ui/src/types/mod.rs
+++ crates/web-ui/src/types/mod.rs
@@ -1,4 +1,4 @@
-use std::ops::{Deref, Range};
+use std::ops::Deref;
 
 #[derive(Debug, Clone)]
 pub(crate) struct Buckets(Vec<Bucket>);
@@ -6,40 +6,43 @@ pub(crate) struct Buckets(Vec<Bucket>);
 #[derive(Debug, Clone)]
 pub(crate) struct Bucket {
     number: u32,
-    range: Range<f64>,
+    le: f64,
 }
 
 impl Buckets {
-    pub fn calculate(initial_value: f64, factor: f64, num_of_buckets: u32) -> Self {
+    pub(crate) fn calculate(initial_value: f64, factor: f64, num_of_buckets: u32) -> Self {
+        // Include last bucket, which is open ended
+        // TODO: use bounded integer type to restrict passing too large values.
+        //      Overflow isn't possible due to validations, but this api is isolated
+        let num_of_buckets = num_of_buckets
+            .checked_add(1)
+            .expect("number of buckets - overflow");
         let mut buckets = Vec::with_capacity(num_of_buckets as usize);
-        let mut current_value = initial_value;
-        for i in 1..num_of_buckets {
-            let next_value = current_value * factor;
-            buckets.push(Bucket::new(i, current_value..next_value));
-            current_value = next_value;
+        let mut next_value = initial_value;
+
+        // starting from second bucket, since first one is already added
+        for bucket_num in 1..num_of_buckets {
+            buckets.push(Bucket::new(bucket_num, next_value));
+            next_value *= factor;
         }
         // last bucket is open ended
-        buckets.push(Bucket::new(num_of_buckets, current_value..f64::INFINITY));
+        buckets.push(Bucket::new(num_of_buckets, f64::INFINITY));
 
         Self(buckets)
     }
 }
 
 impl Bucket {
-    fn new(number: u32, range: Range<f64>) -> Self {
-        Self { number, range }
+    fn new(number: u32, le: f64) -> Self {
+        Self { number, le }
     }
 
     pub fn number(&self) -> u32 {
         self.number
     }
 
-    pub fn start(&self) -> f64 {
-        self.range.start
-    }
-
-    pub fn end(&self) -> f64 {
-        self.range.end
+    pub fn le(&self) -> f64 {
+        self.le
     }
 }
 
