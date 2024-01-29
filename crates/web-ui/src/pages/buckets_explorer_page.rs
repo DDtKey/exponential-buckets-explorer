008d118df55164b8360ad8be0b00f4ea304702a1
Artem Medvedev <i@ddtkey.com>
2024-01-29 20:37:44 +0100

    feat: support copying of URL to clipboard

--- crates/web-ui/src/pages/buckets_explorer_page.rs
+++ crates/web-ui/src/pages/buckets_explorer_page.rs
@@ -7,6 +7,7 @@ use leptos::{component, create_signal, view, IntoView, SignalGet, SignalGetUntra
 use leptos_router::use_query;
 use leptos_router::Params;
 use strum::IntoEnumIterator;
+use wasm_bindgen_futures::JsFuture;
 
 const DEFAULT_INITIAL_VALUE: f64 = 1.0;
 const DEFAULT_FACTOR: f64 = 1.5;
@@ -17,6 +18,7 @@ struct BucketsQueryParams {
     initial_value: Option<f64>,
     factor: Option<f64>,
     buckets_num: Option<u32>,
+    unit: Option<Unit>,
 }
 
 #[component]
@@ -49,6 +51,40 @@ pub(crate) fn BucketsExplorerPage() -> impl IntoView {
         ))
     };
 
+    let copy_link_callback = move |_| {
+        let mut base_url = Some("http://127.0.0.1:8080") // todo: something like option_env!("TRUNK_PUBLIC_URL")?
+            .map(url::Url::parse)
+            .and_then(Result::ok)
+            .expect("misconfiguration: failed to parse URL from Trunk");
+
+        base_url
+            .query_pairs_mut()
+            .append_pair("initial_value", &initial_value.get().to_string())
+            .append_pair("factor", &factor.get().to_string())
+            .append_pair("buckets_num", &buckets_num.get().to_string())
+            .append_pair("unit", &unit.get().to_string());
+
+        if let Some(clipboard) = leptos::window().navigator().clipboard() {
+            use wasm_bindgen_futures::spawn_local;
+            spawn_local(async move {
+                let promise = clipboard.write_text(base_url.as_ref());
+                match JsFuture::from(promise).await {
+                    Ok(_) => {
+                        log::debug!("successfully copied the link to the clipboard");
+                    }
+                    Err(err) => {
+                        log::warn!("failed to copy the link to the clipboard: {err:?}");
+                        let _ = leptos::window()
+                            .alert_with_message("failed to copy the link to the clipboard");
+                    }
+                }
+            });
+        } else {
+            log::warn!("failed to copy the link to the clipboard: clipboard is `None`");
+            let _ = leptos::window().alert_with_message("failed to copy the link to the clipboard");
+        }
+    };
+
     let options: Vec<_> = Unit::iter().collect();
 
     view! {
@@ -77,6 +113,12 @@ pub(crate) fn BucketsExplorerPage() -> impl IntoView {
                         get=unit set=set_unit options=options
                         on_change=update_buckets_callback
                         label="Unit" />
+                </div>
+                <div class="col-md-3 col-sm-auto">
+                    <button type="button" class="btn btn-secondary" on:click=copy_link_callback
+                        title="Copy the link to the clipboard to the page with the current settings">
+                        Share
+                    </button>
                 </div>
             </div>
 
